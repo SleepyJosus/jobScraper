07ae882615c7e3dcd8f1a8b779231252d3d2405d
Joe Gerhard <jlgerhard@gmail.com>
2019-08-31 17:03:50 -0500

    test

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
+<<<<<<< HEAD
+=======
+const fs = require('fs');
+// CHANGE THIS LINE TO CHANGE WHAT INDEED PAGE TO SCRAPE
+const url = 'https://www.indeed.com/jobs?q=Filter&l=Austin%2C+TX&ts=1567286113421&pts=1567277980887&rq=1&fromage=last&newcount=340';
+>>>>>>> 52b4d2b520d12596cf49e2f0ff540383594052a7
 
 (async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.indeed.com/jobs?q=full+stack+developer&l=Austin,+TX&jt=fulltime&explvl=entry_level&sort=date');
+    await page.goto(url);
     const result = await page.evaluate(() => {
+<<<<<<< HEAD
         let nodeArr = [];
         let titleArr = [];
         let query = document.querySelectorAll('div.jobsearch-SerpJobCard')
@@ -29,7 +36,29 @@ const $ = require('cheerio');
                 }
             })
         }        
+=======
+        function cleanString(str) {
+            str = str.split('');
+            if(str[1] == 'n') str.splice(0, 2);
+            str = str.join('').toString().trim();
+            return str;
+        }
+        let titleArr = [];
+        for(let i = 0; i < document.querySelectorAll('.title a').length; i++) {
+            let job = {
+                title: cleanString(document.querySelectorAll('.title a')[i].textContent),
+                company: cleanString(document.querySelectorAll('.company')[i].textContent),
+                desc: cleanString(document.querySelectorAll('.summary')[i].textContent),
+                location: cleanString(document.querySelectorAll('.location')[i].textContent),
+                ratings: cleanString(document.querySelectorAll('.slNoUnderline')[i] == undefined ? 'N/A': document.querySelectorAll('.slNoUnderline')[i].textContent),
+                link: 'https://www.indeed.com' + document.querySelectorAll('.title a')[i].getAttribute('href'),
+            }
+            titleArr.push(job);
+        }
+>>>>>>> 52b4d2b520d12596cf49e2f0ff540383594052a7
         return titleArr;
     })
-    console.log(result);
+    fs.writeFile('jobs.json', JSON.stringify(result), 'utf8', (err) => {
+        if(err) console.error(err);
+    })
 })()
