1c4c0e0f6426134a85daae3a286a52a782cd2e0d
Joe Gerhard <jlgerhard@gmail.com>
2019-08-31 17:00:38 -0500

    working on less brute force scraper format

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-<<<<<<< HEAD
-=======
-const url = 'https://www.indeed.com/jobs?q=full+stack+developer&l=Austin,+TX&jt=fulltime&explvl=entry_level&sort=date';
->>>>>>> 77c7ce371321aa7249b02d6a5048e01a84ac1ae9
 
 (async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto('https://www.indeed.com/jobs?q=full+stack+developer&l=Austin,+TX&jt=fulltime&explvl=entry_level&sort=date');
     const result = await page.evaluate(() => {
+        let nodeArr = [];
         let titleArr = [];
-        for(let i = 0; i < document.querySelectorAll('.title a').length; i++) {
-            console.log(document.querySelectorAll('.title a')[0].textContent)
-            titleArr.push(document.querySelectorAll('.title a')[i].textContent);
-        }
+        let query = document.querySelectorAll('div.jobsearch-SerpJobCard')
+        for(let i = 0; i < query.length; i++) {
+            query[i].childNodes.forEach((node, idx) => {
+                if (node.className) {
+                    if(node.className === 'title') {
+                        let temp = {
+                            title: node.childNodes[1].textContent,
+                            href: node.childNodes[1].href
+                        }
+                        titleArr.push(temp);
+                        // node.childNodes[1]
+                    }
+                    let temp = {
+                        className: node.className,
+                        child: idx 
+
+                    }
+                    nodeArr.push(temp);
+                }
+            })
+        }        
         return titleArr;
     })
     console.log(result);
